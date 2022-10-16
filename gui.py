4ff52da971cc8782603ce00796bfdeb0bfc47ba4
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2022-10-15 22:40:10 -0500

    gui update

--- gui.py
+++ gui.py
@@ -1,9 +1,15 @@
 from tkinter import *
 from tkcalendar import Calendar
+# import tkFont
+
 
 window = Tk()
 
-window.title("Welcome to LikeGeeks app")
+window.title('Campus CO2 Emission Profile') #, font=(18,'Arial'))
+Title = Label(text="Campus CO2 Emission Profile")
+Title.pack()
+Title.configure(font = ("Comic Sans MS", 20, "bold")) #Arial
+window.geometry('920x640')
 
 # Add Calendar
 cal = Calendar(window, selectmode = 'day',
@@ -17,6 +23,11 @@ def grad_date():
 
 # Add Button and Label
 Button(window, text="Get Date", command=grad_date).pack(pady=20)
+
+Button(window, text="Refresh").pack()
+
+# Real-time Auto Refresh
+
 
 date = Label(window, text = "")
 date.pack(pady = 20)

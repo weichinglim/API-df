2e3225a4fd9c4edadf3697c10917c317d2579db1
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2022-10-16 01:46:50 -0500

    completed gui design

--- gui.py
+++ gui.py
@@ -17,15 +17,15 @@ NavigationToolbar2Tk)
 root = Tk()
 
 # Set geometry
-root.geometry("2000x2000")
+root.geometry("1000x600")
 
 fig = Figure(figsize=(5, 4), dpi=100)
 t = np.arange(0, 3, .01)
 fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.grid(column=2,row=0)
 canvas.draw()
+canvas.get_tk_widget().place(anchor=E, x=980, y=300, relheight=0.4, relwidth=0.4)
 
 today = date.today()
 # today.place(x=0, y=0)
@@ -34,7 +34,7 @@ today = date.today()
 cal = Calendar(root, selectmode='day',
                year=2020, month=5,
                day=22)
-cal.grid(column=0, row=0)
+cal.place(anchor=NW, x=20, y=10, relheight=0.2, relwidth=0.2)
 
 
 def button_click():
@@ -47,15 +47,15 @@ def button_click():
 btn = Button(root, text="Get Date",
        command=button_click)
 
-btn.grid(column=1, row=0)
+btn.place(anchor=NW, x=20, y=135, relheight=0.05, relwidth=0.1)
 
 date = Label(root, text="")
-date.grid(column=0, row=1)
+date.place(anchor=NW, x=20, y=280, relheight=0.05, relwidth=0.2)
 
 
 types = ['range', 'date']
 chosen = ttk.Combobox(root, values=types, width=7)
-chosen.grid(column=0, row=2)
+chosen.place(anchor=NW, x=20, y=200, relheight=0.05, relwidth=0.1)
 
 # Execute Tkinter
 root.mainloop()

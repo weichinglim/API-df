9270afe5c83df30b165057486b757e9fb0514f00
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2022-10-16 02:05:38 -0500

    fixed gui design

--- gui.py
+++ gui.py
@@ -34,28 +34,45 @@ today = date.today()
 cal = Calendar(root, selectmode='day',
                year=2020, month=5,
                day=22)
-cal.place(anchor=NW, x=20, y=10, relheight=0.2, relwidth=0.2)
+cal.place(anchor=NW, x=20, y=10, relheight=0.3, relwidth=0.3)
 
-
-def button_click():
+def button_click(): 
     date.config(text="Selected Date is: " + cal.get_date())
     canvas.draw()
 
-
-
 # Add Button and Label
-btn = Button(root, text="Get Date",
+btn = Button(root, text="Confirm/Refresh",
        command=button_click)
 
-btn.place(anchor=NW, x=20, y=135, relheight=0.05, relwidth=0.1)
+btn.place(anchor=NW, x=20, y=220, relheight=0.05, relwidth=0.1)
+
+range1 = Label(root, text="")
+range1.place(anchor=NW, x=20, y=350, relheight=0.05, relwidth=0.15)
+
+range2 = Label(root, text="")
+range2.place(anchor=NW, x=130, y=350, relheight=0.05, relwidth=0.15)
+
+def button_click_range1():
+    range1.config(text= cal.get_date())
+
+def button_click_range2():
+    range2.config(text= cal.get_date())
+
+
+btn_range1 = Button(root, text="RangeStart", command=button_click_range1)
+btn_range1.place(anchor=NW, x=20, y=390, relheight=0.05, relwidth=0.10)
+
+btn_range2 = Button(root, text="RangeEnd", command=button_click_range2)
+btn_range2.place(anchor=NW, x=130, y=390, relheight=0.05, relwidth=0.10)
+
 
 date = Label(root, text="")
-date.place(anchor=NW, x=20, y=280, relheight=0.05, relwidth=0.2)
+date.place(anchor=NW, x=20, y=500, relheight=0.05, relwidth=0.2)
 
 
 types = ['range', 'date']
 chosen = ttk.Combobox(root, values=types, width=7)
-chosen.place(anchor=NW, x=20, y=200, relheight=0.05, relwidth=0.1)
+chosen.place(anchor=NW, x=20, y=300, relheight=0.05, relwidth=0.1)
 
 # Execute Tkinter
 root.mainloop()
